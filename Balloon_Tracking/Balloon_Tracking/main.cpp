0d028a1328cb17020c25da55b4bd8674928308e4
Dar Mehta <dmehta97@gmail.com>
2016-08-06 22:45:39 -0400

    Init commit: setup xcode project, included opencv dependencies.

---
+++ Balloon_Tracking/Balloon_Tracking/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Balloon_Tracking
+//
+//  Created by Dar Mehta on 2016-08-06.
+//  Copyright © 2016 Dar Mehta. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    return 0;
+}
